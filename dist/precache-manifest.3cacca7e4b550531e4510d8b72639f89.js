668a064e8aafd000b0df15f275bcf7faeaa69a0f
Jonathan Peoples <jonathan.peoples@gmail.com>
2020-03-15 18:09:23 -0500

    Adding dist to git

---
+++ dist/precache-manifest.3cacca7e4b550531e4510d8b72639f89.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85016923f0a80fe2ac94",
+    "url": "/css/app.3f74a66b.css"
+  },
+  {
+    "revision": "99e114f1ecef36a085f507d6be3cbaf3",
+    "url": "/img/sliders-h-light.svg"
+  },
+  {
+    "revision": "d4a611b5a918080c09d65841c8ec1dd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2979290258ac3ca48694",
+    "url": "/js/about.90c72beb.js"
+  },
+  {
+    "revision": "85016923f0a80fe2ac94",
+    "url": "/js/app.ed5d7cc8.js"
+  },
+  {
+    "revision": "eae1faa84a19445ede50",
+    "url": "/js/chunk-vendors.fc9d8321.js"
+  },
+  {
+    "revision": "b6f27906ea60b44b13ef31dd204b752e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
